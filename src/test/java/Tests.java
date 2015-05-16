af20a4abe00e3e7f61eae6955496d1990bf3032c
LEOBAINI <leobaini@hotmail.com>
2015-05-15 21:06:27 -0300

    1er

---
+++ src/test/java/Tests.java
@@ -0,0 +1,25 @@
+
+
+
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+
+public class Tests {
+	@Test
+	public void test1(){
+		Assert.assertFalse(false);
+		
+	}
+	@Test
+	public void test2(){
+		Assert.assertTrue(true);
+		
+	}
+	
+	
+	
+
+}
